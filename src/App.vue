ca4da08e3d8a405240eed7e643518175c799397e
Ch3my <35230589+Ch3my@users.noreply.github.com>
2019-12-23 09:44:01 -0300

    Updating packages

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+<!-- Este component es un layout dinamico que se puede definir. cada vista tiene su layout lo que puede dar -->
+<!-- Flexibilidad en la aplicacion -->
   <component :is="layout">
     <router-view :layout.sync="layout"></router-view>
   </component>
